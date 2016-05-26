d5472f0cc2dd60360c0a89be67ba2a5d2307e86c
Mrwaite <chenweite@xiyouant.org>
2016-05-26 22:42:37 +0800

    完成注册页面

---
+++ models/user.js
@@ -0,0 +1,69 @@
+var mongodb = require("./db");
+
+function User(user){
+    this.name = user.name;
+    this.password = user.password;
+    this.email = user.email;
+};
+
+module.exports = User;
+
+//存储用户信息
+//User是描述数据的对象
+//User就是MVC架构里面的模型
+//M  即模型是正真与数据打交道的工具，没有模型数据就是一个空壳
+User.prototype = {
+    save : function(callback) {
+        //要存入数据库的用户信息
+        var user = {
+            name: this.name,
+            password: this.password,
+            email: this.email
+        };
+        //打开数据库
+        mongodb.open(function (err, db) {
+            if (err) {
+                return callback(err);//错误，返回err信息
+            }
+            //读取users 集合
+            db.collection('users', function (err, collection) {
+                if (err) {
+                    mongodb.close();
+                    return callback(err);//错误， 返回err信息
+                }
+                //将用户信息插入用户集合
+                collection.insert(user, {safe: true}, function (err, user) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);//错误，返回err信息
+                    }
+                    callback(null, user[0]);//成功！err为null , 返回存储后用户的文档
+                });
+            });
+        });
+    }
+};
+
+User.get = function(name, callback){
+    //打开数据科
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err);//错误返回错误信息
+        }
+        //读物users 集合
+        db.collection('users', function(err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);//错误，返回err信息
+            }
+            //查找用户名（name键）值为 name 一个文档
+            collection.findOne({name : name}, function(err, user){
+                mongodb.close();
+                if(err){
+                    return callback(err);//错误，返回err信息
+                }
+                callback(null , user);//成功!  返回查询的用户信息
+            });
+        });
+    });
+};
---
+++ routes/index.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+
+/*
+/  首页
+/login 用户登录
+/reg 用户注册
+/post 发表文章
+/logout 登出
+*/
+
+//导入crypto模块，user.js用户模型文件，crypto用来生成散列值来加密密码
+var crypto = require("crypto"),
+    User = require("../models/user.js");
+
+module.exports = function(app){
+  app.get('/',function(req,res){
+    res.render('index', {title : '主页'});
+  });
+    app.get('/reg',function(req,res){
+       res.render('reg',{title: '注册'});
+    });
+    app.post('/reg',function(req,res){
+        //通过body-parser middleware 来读取传过来的json数据
+        var name = req.body.name,
+            password = req.body.password,
+            password_re = req.body['password-repeat'],
+            email = req.body.email;
+        //检验用户两次输入的密码是否一直
+        if(password_re !== password){
+            req.flash('error', '，两次输入的密码不一致');
+            return res.redirect('/reg');
+        }
+        //生成密码的MD5值
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('hex');
+        var newUser = new User({
+            name : name,
+            password : password,
+            email : email
+        });
+        //检查用户名是否存在
+        User.get(newUser.name, function(err, user){
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            if(user){
+                req.flash('error', '用户已存在');
+                return res.redirect('/reg');//如果用户名不存在，返回注册页面
+            }
+            //把用户信息存储到session里，以后就可以通过req.session.user读取用户信息
+            newUser.save(function(err, user){
+                if(err){
+                    req.flash('err', err);
+                    return res.redirect('reg');//注册失败返回注册页
+                }
+                req.session.user = newUser;
+                req.flash('success', '注册成功');
+                //redirect重定向功能
+                res.redirect('/');
+            });
+
+        });
+    });
+    app.get('/login',function(req,res){
+        res.render('login',{title : '登陆'});
+    });
+    app.post('/login',function(req,res){
+
+    });
+   app.get('/post',function(req,res){
+       res.render('post',{title: '发表'});
+   });
+    app.post('/post',function(req,res){
+
+    });
+    app.get('/loginout',function(req,res){
+
+    });
+};
